1fa079dc882ef1ed08856f440cc419cad1a947af
peer23peer <spijker.jelle@gmail.com>
2015-08-02 11:46:39 +0200

    Serialization of the Predict_t type for Boost added

---
+++ src/SoilMath/predict_t_archive.h
@@ -0,0 +1,31 @@
+/* Copyright (C) Jelle Spijker - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * and only allowed with the written consent of the author (Jelle Spijker)
+ * This software is proprietary and confidential
+ * Written by Jelle Spijker <spijker.jelle@gmail.com>, 2015
+ */
+
+// Source:
+// http://stackoverflow.com/questions/16125574/how-to-serialize-opencv-mat-with-boost-xml-archive
+#pragma once
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/vector.hpp>
+#include "SoilMathTypes.h"
+
+namespace boost {
+namespace serialization {
+/*!
+ * \brief serialize Serialize the openCV mat to disk
+ */
+template <class Archive>
+inline void serialize(Archive &ar, Predict_t &P, const unsigned int version __attribute__((unused))) {
+  ar &P.Accuracy;
+  ar &P.Category;
+  ar &P.OutputNeurons;
+  ar &P.RealValue;
+}
+}
+}
