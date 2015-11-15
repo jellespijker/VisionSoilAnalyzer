d92a8a67371c7cd63facd642d8cf7b8cec62179e
peer23peer <spijker.jelle@gmail.com>
2015-11-15 17:43:04 +0100

    Build script update

--- LinuxScripts/build.sh
+++ LinuxScripts/build.sh
@@ -1,76 +1,76 @@
- 
 #!bin/bash
 #script to build Vision Soil Analyzer
 
-
 START=$(date +%s)
 
-echo "Building Vision Soil Analyzer"
-setupbuilddir.sh
+config = $1
 
-cd ../build/debug/SoilMath/
+echo "Building Vision Soil Analyzer"
+./setupbuilddir.sh
+
+cd ../build/$config/SoilMath/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/SoilMath/SoilMath.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/SoilMath/SoilMath.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
 cd ../SoilHardware/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/SoilHardware/SoilHardw.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/SoilHardware/SoilHardw.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
 cd ../SoilVision/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/SoilVision/SoilVision.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/SoilVision/SoilVision.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
 cd ../SoilAnalyzer/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/SoilAnalyzer/SoilAnalyzer.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/SoilAnalyzer/SoilAnalyzer.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
 cd ../../release/pictureflow-qt/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/pictureflow/lib.pro -r -spec linux-g++
+qmake ../../../src/pictureflow/lib.pro -r -spec linux-g++
 make
 make install
 
 cd ../qcustomplot/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/qcustomplot/qcustomplot.pro -r -spec linux-g++
+qmake ../../../src/qcustomplot/qcustomplot.pro -r -spec linux-g++
 make
 make install
 
-cd ../../debug/QOpenCVQT/
+cd ../../$config/QOpenCVQT/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/QOpenCVQT/QOPEnCVQT.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/QOpenCVQT/QOPEnCVQT.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
 cd ../QParticleDisplay/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/QParticleDisplay/QParticleDisplay.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/QParticleDisplay/QParticleDisplay.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
 cd ../QParticleSelector/
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/QParticleSelector/QParticleSelector.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/QParticleSelector/QParticleSelector.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
 cd ../QReportGenerator
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/QReportGenerator/QReportGenerator.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/QReportGenerator/QReportGenerator.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
 cd ../VSA
 rm *
-qmake /home/peer23peer/ihc/VisionSoilAnalyzer/src/VSA/VSA.pro -r -spec linux-g++ CONFIG+=debug
+qmake ../../../src/VSA/VSA.pro -r -spec linux-g++ CONFIG+=$config
 make
 make install
 
