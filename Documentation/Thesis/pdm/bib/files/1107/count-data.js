5979060eb6e0fb9c553d2a2a6fdba2766ba62a39
peer23peer <spijker.jelle@gmail.com>
2015-11-18 21:39:04 +0100

    Removed citation linkes

--- Documentation/Thesis/pdm/bib/files/1107/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"7989 http:\/\/beyondplm.com\/?p=7989","comments":0}]});
-}
